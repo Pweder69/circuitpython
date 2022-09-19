4cab6e5bc123953b9fa096dcd16126eb38e46c13
pweder69 <pweder07@charlottesvilleschools.org>
2022-09-19 11:23:59 -0400

    finished button

--- code/buttonServo.py
+++ code/buttonServo.py
@@ -1,3 +1,4 @@
+
 import board
 import time
 import pwmio
@@ -9,24 +10,33 @@ pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
 btn1 = DigitalInOut(board.D8)
 btn1.direction = Direction.INPUT
 btn1.pull = Pull.UP
+#definitions for button pins 
 
 btn2 = DigitalInOut(board.D9)
 btn2.direction = Direction.INPUT
 btn2.pull = Pull.UP
 
-con180 =lambda x:10 if (x>=180 or x<=0) else x 
 
 my_servo = servo.Servo(pwm)
 angle = 0
+lock1 = False
+lock2 = False
+
 
 while True:
-    if not  btn2.value:
-        angle += con180(angle+1)
-        my_servo.angle = angle
-        
-    if not btn1.value:
-        angle -= con180(angle+1)
-        my_servo.angle = angle
-        
+    if not  btn2.value and lock1 == False:
+        lock1 = True
+        lock2 = False
+        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+            print(angle)
+
+
+    if not btn1.value and lock2 == False:
+        lock1 = False # locks the turn so that you dont have to 
+        lock2 = True
+        for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+            print(angle)
 
     time.sleep(0.1) # sleep for debounce
