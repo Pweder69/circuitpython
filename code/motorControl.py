ce2c59b2c9441b340197b884d1053fb3376a915a
pweder69 <pweder07@charlottesvilleschools.org>
2022-11-03 10:50:16 -0400

    finial motor control

--- code/motorControl.py
+++ code/motorControl.py
@@ -1,15 +1,10 @@
 import board
-import time
-import pwmio
 from digitalio import DigitalInOut, Direction, Pull
-import simpleio
 from analogio import AnalogIn, AnalogOut 
 
 control = AnalogOut(board.A1)
 
-
 pot = AnalogIn(board.A0)
-
 
 while True:
     print(int(pot.value))
